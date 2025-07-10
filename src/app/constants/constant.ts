b821c6d070abe372bab1f472a16ab42b5c3b5847
zeeshanahm-ed <zeeshan.dev@irsolutions.tech>
2025-07-10 17:38:28 +0500

    static data

---
+++ src/app/constants/constant.ts
@@ -0,0 +1,20 @@
+import { Category } from "../utils/Interfaces";
+
+export const categories: Category[] = [
+    { name: 'Horror Movies', icon: '/categories-images/horror-movies.png', color: 'bg-orange' },
+    { name: 'Cinema', icon: '/categories-images/cinema.png', color: 'bg-orange' },
+    { name: 'Wrestling', icon: '/categories-images/wrestling.png', color: 'bg-red' },
+    { name: 'Currency', icon: '/categories-images/currency.png', color: 'bg-orange' },
+    { name: 'Qatar', icon: '/categories-images/qatar.png', color: 'bg-orange' },
+    { name: 'Cars', icon: '/categories-images/cars.png', color: 'bg-orange' },
+    { name: 'Moto Bikes', icon: '/categories-images/motor-bike.png', color: 'bg-red' },
+    { name: 'Football', icon: '/categories-images/football.png', color: 'bg-orange' },
+    { name: 'Fast Food', icon: '/categories-images/fast-food.png', color: 'bg-orange' },
+    { name: 'Saudi Arabia', icon: '/categories-images/saudi-arabia.png', color: 'bg-orange' },
+    { name: 'Basket Ball', icon: '/categories-images/basket-ball.png', color: 'bg-orange' },
+    { name: 'General Info', icon: '/categories-images/general-info.png', color: 'bg-orange' },
+    { name: 'Music', icon: '/categories-images/music.png', color: 'bg-orange' },
+    { name: 'E Gaming', icon: '/categories-images/e-gaming.png', color: 'bg-orange' },
+    { name: 'Pro Boxing', icon: '/categories-images/pro-boxing.png', color: 'bg-orange' },
+    { name: 'Animal Planet', icon: '/categories-images/animal-planet.png', color: 'bg-orange' },
+];
---
+++ src/app/utils/Interfaces.ts
@@ -0,0 +1,5 @@
+export interface Category {
+    name: string;
+    icon: string;
+    color: string;
+}
