8d01ae7d333966f3426edd55f6670a655958d5ca
zeeshanahm-ed <zeeshan.dev@irsolutions.tech>
2025-07-11 17:05:06 +0500

    create join room modal

---
+++ src/app/components/modals/join-room-modal.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation';
+import { MdClose } from 'react-icons/md';
+import Button from '../ui/common/Button';
+import Input from '../ui/common/Input';
+
+function JoinRoomModal({ open, onClose }: any) {
+    const [roomCode, setRoomCode] = useState("");
+
+    return (
+        <dialog id="create_game_modal" className={` modal ${open ? 'modal-open' : ''}`}>
+            <div className=" modal-box bg-white items-center">
+                <form method="dialog " className="flex items-center justify-center relative">
+                    <h2 className="text-7xl font-popfun uppercase">
+                        join room
+                    </h2>
+                    <button
+                        type="button"
+                        className="absolute right-2 top-0 bg-light-gray focus:outline-none w-8 h-8 flex items-center justify-center rounded-full text-white hover:bg-dark-gray transition-colors duration-300"
+                        onClick={onClose}
+                        aria-label="Close"
+                    >
+                        <MdClose className='text-2xl' />
+                    </button>
+                </form>
+                <div className="divider before:bg-light-gray after:bg-light-gray m-0"></div>
+                {/* Main content */}
+                <div className="flex items-center flex-col w-full justify-between px-10 py-5">
+                    <p className='text-lg font-Product_sans'>Enter Room code to participate in it.</p>
+                    <Input
+                        type="text"
+                        placeholder="Enter Room Code"
+                        value={roomCode}
+                        className='my-10  w-full'
+                        inputClassName='text-center pl-0'
+                        onChange={(e) => setRoomCode(e.target.value)}
+                    />
+                    <Button className='text-4xl w-64 tracking-wide'>Join now</Button>
+                </div>
+            </div>
+        </dialog>
+    )
+}
+
+export default JoinRoomModal;
