e4baf7edee72b7e8b28a0992564d8318c90211ac
zeeshanahm-ed <zeeshan.dev@irsolutions.tech>
2025-07-11 17:06:38 +0500

    categories section selected toggle functionality add

--- src/app/components/ui/common/CategoriesSection.tsx
+++ src/app/components/ui/common/CategoriesSection.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Input from '@/app/components/ui/common/Input';
 import Image from 'next/image';
 import { Category } from '@/app/utils/Interfaces';
@@ -13,35 +13,63 @@ import SearchIcon from "../../../assets/icons/search-icon.svg"
 
 function CategoriesSection() {
     const [search, setSearch] = useState('');
+    const [filteredCategories, setFilteredCategories] = useState<Category[]>(categories);
+
+    useEffect(() => {
+        const modifyCategories = categories?.map((item) => {
+            return {
+                ...item,
+                selected: false
+            }
+        });
+        setFilteredCategories(modifyCategories);
+    }, [categories])
 
     const handleSearch = (value: string) => {
         setSearch(value)
     };
 
+    const handleCategoriesClick = (value: Category) => {
+        const modifiedCategories = filteredCategories.map((item) => {
+            if (item.name === value.name) {
+                return {
+                    ...item,
+                    selected: !item.selected
+                }
+            } else {
+                return { ...item }
+            }
+        }
+        );
+        setFilteredCategories(modifiedCategories);
+        const selectedCategory = modifiedCategories.find((item) => item.selected);
+        localStorage.setItem("selectedCategories", JSON.stringify(selectedCategory))
+    };
+
     return (
-        <div className='flex items-center justify-center flex-col w-full px-10'>
+        <div className='flex items-center justify-center flex-col w-full md:px-10 px-4'>
             <div className='my-10'>
                 <Input
                     icon={<SearchIcon />}
                     type="text"
                     value={search}
                     placeholder="Search by category Name"
-                    className='w-96'
+                    className='md:w-96 w-full'
                     onChange={(e) => handleSearch(e.target.value)}
                 />
             </div>
             <div className="text-center flex flex-col items-center justify-center">
-                <h2 className="lg:text-8xl text-6xl font-popfun text-black mb-2 uppercase">
+                <h2 className="sm:text-6xl text-5xl lg:text-7xl font-popfun text-black mb-2 uppercase">
                     Select your categories
                 </h2>
-                <p className="text-lg leading-6 text-black max-w-2xl">
+                <p className="text-sm sm:text-base md:text-lg leading-6 text-black max-w-2xl">
                     3 categories for your team, and 3 categories for the opposing team, for a total of 6 categories with 36 different questions. Choose the categories carefully to ensure the greatest chance of
                 </p>
             </div>
-            <div className='my-14 w-full'>
-                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 gap-y-10">
-                    {categories.map((cat, idx) => (
-                        <CategoryCard key={idx} category={cat} />
+            <div className='my-14 w-full max-h-[800px] min-h-[600px] overflow-y-auto'>
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 gap-y-10">
+                    {filteredCategories?.map((cat, idx) => (
+                        <CategoryCard key={idx} category={cat} handleCategoriesClick={handleCategoriesClick} />
                     ))}
                 </div>
             </div>
@@ -53,15 +81,16 @@ export default CategoriesSection;
 
 interface Props {
     category: Category;
+    handleCategoriesClick: (category: any) => void;
 }
 
-const CategoryCard = ({ category }: Props) => {
+const CategoryCard = ({ category, handleCategoriesClick }: Props) => {
     return (
-        <div className="flex flex-col justify-center items-center">
-            <div className="relative w-44 h-44 rounded-full border-[12px] border-orange flex items-center justify-center">
-                <Image src={category.icon} alt={category.name} width={100} height={100} />
+        <div className="flex cursor-pointer flex-col justify-center items-center" onClick={() => handleCategoriesClick(category)}>
+            <div className={`relative w-28 h-28 sm:w-36 sm:h-36 md:w-44 md:h-44 rounded-full border-[12px] border-orange ${category.selected ? "border-red" : "border-orange"} flex items-center justify-center`}>
+                <Image src={category.icon} alt={category.name} width={100} height={100} className='w-1/2 h-1/2' />
             </div>
-            <div className={`w-full mt-6 relative px-4 pb-1 pt-2 text-center text-white font-popfun text-2xl md:text-4xl uppercase tracking-wide ${category.color} 
+            <div className={`w-full mt-6 relative px-4 pb-1 pt-2 text-center text-white font-popfun text-2xl sm:text-3xl md:text-4xl uppercase tracking-wide ${category.selected ? "bg-red" : "bg-orange"} 
             before:w-3 before:bg-orange before:h-8 before:absolute before:-top-8 before:left-[47%] `}>
                 {category.name}
             </div>
