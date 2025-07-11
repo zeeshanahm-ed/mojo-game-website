016318e4a74d4ae19f9630d3a1e06575866cce15
zeeshanahm-ed <zeeshan.dev@irsolutions.tech>
2025-07-11 17:11:12 +0500

    contact page form handling add

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React, { useState } from 'react'
+import { useCountries } from '../hooks/useCountries';
 
 import Wrapper from '@/app/components/ui/common/Wrapper';
 import Banner from './components/Banner';
@@ -8,19 +9,33 @@ import Button from '@/app/components/ui/common/Button';
 
 
 //icons
-import PasswordIcon from '@/app/assets/icons/password-icon.svg';
+import CountryCodeIcon from '@/app/assets/icons/country-code-icon.svg';
 import EmailIcon from '@/app/assets/icons/email-icon.svg';
-import EditIcon from '@/app/assets/icons/edit-icon.svg';
 import ContactIcon from '@/app/assets/icons/contact-icon.svg';
 import UserIcon from '@/app/assets/icons/user-icon.svg';
+import Select from '../components/ui/common/Select';
 
 
 function ContactUS() {
+    const { countries, loading, error } = useCountries();
 
-    const handleSend = (e: React.FormEvent) => {
-        e.preventDefault();
-        console.log('Sign Up button clicked!');
+    const [form, setForm] = useState({
+        fullName: '',
+        email: '',
+        contact: '',
+        message: '',
+        countryCode: '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
     };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(form);
+    };
+
 
     return (
         <section>
@@ -28,7 +43,7 @@ function ContactUS() {
             <Wrapper>
                 <Wrapper>
                     <div className='h-auto pt-20 pb-20'>
-                        <form onSubmit={handleSend} className="flex flex-col items-center p-6 space-y-6 w-full">
+                        <form onSubmit={handleSubmit} className="flex flex-col items-center p-6 space-y-6 w-full">
                             {/* First Name Input */}
                             <Input
                                 icon={<UserIcon />}
@@ -36,6 +51,9 @@ function ContactUS() {
                                 placeholder="Full Name"
                                 className="w-1/2 mb-0"
                                 required
+                                name="fullName"
+                                onChange={handleChange}
+                                value={form.fullName}
                             />
 
                             {/* Email Input */}
@@ -45,6 +63,19 @@ function ContactUS() {
                                 placeholder="Enter your email address"
                                 className="w-1/2"
                                 required
+                                name="email"
+                                onChange={handleChange}
+                                value={form.email}
+                            />
+
+                            <Select
+                                icon={<CountryCodeIcon />}
+                                name="countryCode"
+                                value={form.countryCode}
+                                onChange={handleChange}
+                                options={countries || []}
+                                placeholder="Select Countery Code"
+                                className="w-1/2"
                             />
 
                             {/* Contact Number Input */}
@@ -54,11 +85,18 @@ function ContactUS() {
                                 placeholder="Your Contact Number"
                                 className="w-1/2"
                                 required
+                                name="contact"
+                                onChange={handleChange}
+                                value={form.contact}
                             />
 
                             <textarea
-                                className="w-1/2 h-[200px] outline-none resize-none bg-white border-2 border-black skew-custom p-5 text-lg"
-                                placeholder="Your message"></textarea>
+                                className="w-1/2 h-[200px] outline-none resize-none bg-white border-2 border-black -skew-x-12 p-5 text-lg"
+                                placeholder="Your message"
+                                name="message"
+                                onChange={handleChange}
+                                value={form.message}>
+                            </textarea>
 
                             {/* SignUp Button */}
                             <div className='flex items-center justify-center mt-10'>
