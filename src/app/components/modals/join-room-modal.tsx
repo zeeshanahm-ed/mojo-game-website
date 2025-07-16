b4c09a9230230400fb0cc70860cd360f9390e843
zeeshanahm-ed <zeeshan.dev@irsolutions.tech>
2025-07-16 17:15:33 +0500

    refactor: remove unused useRouter import in JoinRoomModal component

--- src/app/components/modals/join-room-modal.tsx
+++ src/app/components/modals/join-room-modal.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { useRouter } from 'next/navigation';
 import { MdClose } from 'react-icons/md';
 import Button from '../ui/common/Button';
 import Input from '../ui/common/Input';
