943e98f942ef50daba8ff5043b27074565583ac2
zeeshanahm-ed <zeeshan.dev@irsolutions.tech>
2025-07-22 11:03:02 -0700

    Add scroll to top component

--- src/app/ClientLayoutWrapper.tsx
+++ src/app/ClientLayoutWrapper.tsx
@@ -3,6 +3,7 @@
 import { usePathname } from 'next/navigation';
 import Header from './components/ui/common/Header';
 import Footer from './components/ui/common/Footer';
+import ScrollToTop from './components/utils/ScrollToTop';
 
 export default function ClientLayoutWrapper({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
@@ -11,7 +12,7 @@ export default function ClientLayoutWrapper({ children }: { children: React.Reac
     return (
         <div className="min-h-screen flex flex-col">
             {pathname !== hideHeaderOn && <Header />}
-
+            <ScrollToTop />
             <main className="flex-grow">
                 {children}
             </main>
