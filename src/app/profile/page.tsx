1b3a6b99b45d80df20b431c287c74216c2efa8a2
zeeshanahm-ed <zeeshan.dev@irsolutions.tech>
2025-08-11 17:35:09 +0500

    trabslation add

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useCountries } from '../hooks/useCountries';
 
 import Wrapper from '@/app/components/ui/common/Wrapper';
@@ -20,38 +20,90 @@ import ContactIcon from '@/app/assets/icons/contact-icon.svg';
 import UserIcon from '@/app/assets/icons/user-icon.svg';
 import EditIcon from '@/app/assets/icons/edit-icon.svg';
 import FallBackProfileImage from '@/app/assets/images/fallback-profile-image.jpg';
+import { useAuth } from '../context/AuthContext';
+import { useRouter } from 'next/navigation';
+import { showErrorMessage, showSuccessMessage } from '../utils/messageUtils';
+import useUpdateUserProfile from './core/hooks/useUpdateUserProfile';
 
-
+interface FormState {
+    firstName: string,
+    lastName: string,
+    email: string,
+    phoneNumber: string,
+    avatar: string | null,
+    countryCode: string,
+}
 
 function Profile() {
     const { countries } = useCountries();
     const { openModal } = useAuthModalStore();
     const { t } = useTranslation();
     const direction = useDirection();
+    const { user } = useAuth();
+    const router = useRouter();
+    const { mutateUpdateUserProfile } = useUpdateUserProfile();
+    const [profilePicObj, setProfilePicObj] = useState<File | null>(null);
 
-    const [form, setForm] = useState({
-        fullName: '',
+    const [formState, setFormState] = useState<FormState>({
+        firstName: '',
+        lastName: '',
         email: '',
-        contact: '',
-        profilePicture: '',
+        phoneNumber: '',
+        avatar: '',
         countryCode: '',
     });
 
+    useEffect(() => {
+        if (user) {
+            setFormState(prev => (
+                {
+                    ...prev,
+                    firstName: user.data.firstName,
+                    lastName: user.data.lastName,
+                    email: user.data.email,
+                    avatar: user.data.imageUrl,
+                    phoneNumber: user.data.phoneNumber
+                }
+            ));
+        }
+        else {
+            router.push("/")
+        }
+    }, [user])
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
-        setForm({ ...form, [e.target.name]: e.target.value });
+        setFormState({ ...formState, [e.target.name]: e.target.value });
     };
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        console.log(form);
+    const handleProfileChange = () => {
+        const formData = new FormData();
+        formData.append("firstName", formState.firstName);
+        formData.append("lastName", formState.lastName);
+        formData.append("email", formState.email);
+        formData.append("phoneNumber", formState.countryCode + formState.phoneNumber);
+
+        if (formState.avatar && profilePicObj instanceof File) {
+            formData.append("avatar", profilePicObj);
+        }
+
+        mutateUpdateUserProfile(formData, {
+            onSuccess: async () => {
+                showSuccessMessage('Profile update successful.');
+            },
+            onError: (error: any) => {
+                showErrorMessage(error?.response?.data?.message);
+                console.error('Failed to update rofile:', error);
+            },
+        });
     };
 
     const handleProfilePictureChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
         if (file) {
+            setProfilePicObj(file);
             const reader = new FileReader();
             reader.onloadend = () => {
-                setForm((prev) => ({ ...prev, profilePicture: reader.result as string }));
+                setFormState((prev) => ({ ...prev, avatar: reader.result as string }));
             };
             reader.readAsDataURL(file);
         }
@@ -83,13 +135,13 @@ function Profile() {
                             <input
                                 type="file"
                                 ref={fileInputRef}
-                                name="profilePicture"
+                                name="avatar"
                                 onChange={handleProfilePictureChange}
                                 accept="image/*"
                                 className="hidden"
                             />
                             <Image
-                                src={form.profilePicture || (typeof FallBackProfileImage === 'string' ? FallBackProfileImage : FallBackProfileImage.src)}
+                                src={formState.avatar || (typeof FallBackProfileImage === 'string' ? FallBackProfileImage : FallBackProfileImage.src)}
                                 alt="User Profile"
                                 className="w-full h-full object-cover"
                                 width={160}
@@ -108,7 +160,7 @@ function Profile() {
                         </Button>
                     </div>
 
-                    <form onSubmit={handleSubmit} className="flex flex-col items-center sm:p-6 space-y-6">
+                    <div className="flex flex-col items-center sm:p-6 space-y-6">
 
                         {/* First Name & Last Name Input */}
                         <div dir={direction} className="flex font-secondary items-center h-14 sm:w-[450px] lg:w-full w-full transform -skew-x-6 md:-skew-x-12 border-2 border-black overflow-hidden">
@@ -117,6 +169,7 @@ function Profile() {
                             </div>
                             <input
                                 type="text"
+                                value={formState.firstName}
                                 name='firstName'
                                 placeholder={t("firstName")}
                                 className="flex-1 input h-full rounded-none input-bordered  pl-2 md:pl-8 pr-2 text-lg bg-white text-gray-800 border-none focus:outline-none"
@@ -125,6 +178,7 @@ function Profile() {
                             <div className='h-[90%] w-[2px] bg-light-gray'></div>
                             <input
                                 type="text"
+                                value={formState.lastName}
                                 name='lastName'
                                 placeholder={t("lastName")}
                                 className="flex-1 input h-full rounded-none input-bordered  pl-2 md:pl-8 pr-2 text-lg bg-white text-gray-800 border-none focus:outline-none border-l border-gray-300"
@@ -139,16 +193,16 @@ function Profile() {
                             placeholder={t("emailPlaceholder")}
                             className="sm:w-[450px] lg:w-full w-full"
                             required
+                            value={formState.email}
                             name="email"
                             onChange={handleChange}
-                            value={form.email}
                         />
 
                         <Select
                             icon={<CountryCodeIcon />}
                             isCountrySelect={true}
                             name="countryCode"
-                            value={form.countryCode}
+                            value={formState.countryCode}
                             onChange={handleChange}
                             options={CountriesList || []}
                             placeholder={t("selectCountryCode")}
@@ -160,21 +214,21 @@ function Profile() {
                         <Input
                             icon={<ContactIcon />}
                             type="number"
-                            placeholder={t("contactNumber")}
+                            placeholder={t("phoneNumber")}
                             className="sm:w-[450px] lg:w-full w-full"
                             required
                             name="contact"
                             onChange={handleChange}
-                            value={form.contact}
+                            value={formState.phoneNumber}
                         />
 
                         {/* SignUp Button */}
                         <div className='flex items-center justify-center mt-10'>
-                            <Button type="submit" aria-label="Save" className="w-52" boxShadow={false}>
+                            <Button onClick={handleProfileChange} aria-label="Save" className="w-52" boxShadow={false}>
                                 <span className="inline-block transform skew-x-12 tracking-wider text-4xl uppercase ">{t("saveChanges")}</span>
                             </Button>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </Wrapper>
         </section >
